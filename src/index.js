c0fba29cd8f8909f6692cc435a31c6d67d31008a
hkuk1004 <hkuk1004@gmail.com>
2023-09-07 17:23:27 +0900

    fix

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import {
-  BrowserRouter,
-  Routes,
-  Route,
-} from "react-router-dom";
+// import {
+//   BrowserRouter,
+//   Routes,
+//   Route,
+// } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import MyInfoMarket from './components/MyInfoMarket/MyInfoMarket';
 import Technology from "./components/Technology/Technology";
 import Service from "./components/Service/Service";
@@ -15,16 +16,16 @@ import Footer from "./components/Footer/Footer";
 import Recruit from "./components/Recruit/Recruit";
 
 ReactDOM.render(
-  <BrowserRouter>
+  <Router>
     <Navbar />
     <Routes>
-      <Route path="/" element={<MyInfoMarket />} />
-      <Route path="/technology" element={<Technology />} />
-      <Route path="/service" element={<Service />} />
-      <Route path="/company" element={<Company />} />
-      <Route path="/recruit" element={<Recruit />} />
+      <Route path="/" Component={<MyInfoMarket />} />
+      <Route path="/technology" Component={<Technology />} />
+      <Route path="/service" Component={<Service />} />
+      <Route path="/company" Component={<Company />} />
+      <Route path="/recruit" Component={<Recruit />} />
     </Routes>
     <Footer />
-  </BrowserRouter>,
+  </Router>,
   document.getElementById('root')
 );
