d66130d9d7d8235d19664ce416020ec10df792e3
hkuk1004 <hkuk1004@gmail.com>
2023-10-31 11:02:55 +0900

    테스트를 위한 commit

--- src/components/MyInfoMarket/MyInfoMarketBlog.jsx
+++ src/components/MyInfoMarket/MyInfoMarketBlog.jsx
@@ -2,7 +2,8 @@ import React from "react";
 import styled from "styled-components";
 import { NoteSansMedium, GmarketBold } from "../../common/Text/Text";
 import { PRIMARY_COLOR, GREY_FONT_COLOR } from "../../common/color/color";
-
+// 테스트를 위한 주석입니다
+// 삭제해도 무방합니다
 const BlogContainer = styled.div`
     display:flex;
     width: 100%;
